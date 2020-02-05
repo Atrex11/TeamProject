187412161e15881e5611d249454bdc020479473f
Marcus <martinetmarcus@gmail.com>
2020-02-04 22:51:59 -0500

    movment

---
+++ Rogue/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizonlatMove = 0;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input.GetAxisRaw("Horizontal");
+
+        horizonlatMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) { jump = true; }
+    }
+
+    void FixedUpdate(){
+        // Move the character
+        controller.Move(horizonlatMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
